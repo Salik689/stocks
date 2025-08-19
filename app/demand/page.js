f85dd40785e6a688b9f4096d254610cf9a296228
salik <salikahmed6809@gmail.com>
2025-08-19 18:52:55 +0100

    excel

--- app/demand/page.js
+++ app/demand/page.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
-const Demand = () => {
+export default function ExcelIframe() {
   return (
-    <div>
-        <Navbar/>
-      <iframe width="100%" height="685px" frameBorder="0" scrolling="no" src="https://1drv.ms/x/c/ffb62b4907d16059/IQTKRxFMqmM3Q4X3qFyhpPkqAcd44KIrFeQK8_plu9gvFRw?wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True"></iframe>
+    <>
+    <Navbar />
+        <div style={{ width: "100%", overflowX: "auto", padding: "10px" }}>
+      <iframe
+        width="100%"        // full width
+        height="800"        // adjust height as needed
+        frameBorder="0"     // React JSX requires camelCase
+        scrolling="no"      // optional
+        src="https://1drv.ms/x/c/ffb62b4907d16059/IQTKRxFMqmM3Q4X3qFyhpPkqAcd44KIrFeQK8_plu9gvFRw?wdAllowInteractivity=True&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True"
+      ></iframe>
     </div>
-  )
-}
 
-export default Demand
+    </>
+  );
+}
