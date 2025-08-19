c78262e0f091d7ce91c0b3c2e9a32e4f099e76b4
salik <salikahmed6809@gmail.com>
2025-08-19 18:42:33 +0100

    excell

--- app/demand/page.js
+++ app/demand/page.js
@@ -1,12 +1,12 @@
-"use client";
+import React from 'react'
 import Navbar from '../components/Navbar';
-export default function PdfViewer() {
+const Demand = () => {
   return (
-    <>
-    <Navbar/>
-    <div style={{ height: "100vh" }}>
-     <iframe width="100%" height="100%" frameborder="0" scrolling="no" src="https://1drv.ms/x/c/ffb62b4907d16059/IQTKRxFMqmM3Q4X3qFyhpPkqAcd44KIrFeQK8_plu9gvFRw?wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True"></iframe>
+    <div>
+        <Navbar/>
+      <iframe width="100%" height="685px" frameBorder="0" scrolling="no" src="https://1drv.ms/x/c/ffb62b4907d16059/IQTKRxFMqmM3Q4X3qFyhpPkqAcd44KIrFeQK8_plu9gvFRw?wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True"></iframe>
     </div>
-    </>
-  );
+  )
 }
+
+export default Demand
