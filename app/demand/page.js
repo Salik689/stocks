ee5f9767b26a5f2ffb2182906315e588e69e777c
salik <salikahmed6809@gmail.com>
2025-08-19 17:39:25 +0100

    excel

--- app/demand/page.js
+++ app/demand/page.js
@@ -5,12 +5,7 @@ export default function PdfViewer() {
     <>
     <Navbar/>
     <div style={{ height: "100vh" }}>
-      <iframe
-        src="/data/myfile.pdf" // path inside /public/
-        width="100%"
-        height="100%"
-        style={{ border: "none" }}
-      />
+     <iframe width="100%" height="100%" frameborder="0" scrolling="no" src="https://1drv.ms/x/c/ffb62b4907d16059/IQTKRxFMqmM3Q4X3qFyhpPkqAcd44KIrFeQK8_plu9gvFRw?wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True"></iframe>
     </div>
     </>
   );
