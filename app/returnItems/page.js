06684edd5c7d186e9f0bef3d72498e83a1525d40
salik <salikahmed6809@gmail.com>
2025-08-31 13:31:49 +0100

    remove getitem css from return

--- app/returnItems/page.js
+++ app/returnItems/page.js
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import './getItem.css'; // You can rename this to returnItem.css if needed
+
 
 const Page = () => {
   const [stock, setStock] = useState([]);
